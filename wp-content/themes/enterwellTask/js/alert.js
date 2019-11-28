e2002ae4385565dee3a17e00a83b8fcea010ab61
Ferdo Vukojević <f.vukojevic"globalsoft.ba>
2019-11-28 22:44:19 +0100

    alert part 2

--- wp-content/themes/enterwellTask/js/alert.js
+++ wp-content/themes/enterwellTask/js/alert.js
@@ -5,9 +5,25 @@ document.addEventListener("DOMContentLoaded", function() {
 
     if(searchParams.has('msg')) {
         let msg = searchParams.get('msg');
+        let errorAlert = document.getElementById('error-alert')
+        let sucessAlert = document.getElementById('success-alert')
+
         if(msg !== 'success') {
             let errorMsg = document.getElementById('error-msg');
             errorMsg.innerHTML = msg
+            errorAlert.style.zIndex = "999";
+            sucessAlert.style.zIndex = "-1";
+            let errorBtn = document.getElementById('error-img');
+            errorBtn.onclick = function(){
+                errorAlert.style.zIndex = "-1";
+            }
+        } else {
+            sucessAlert.style.zIndex = "999";
+            errorAlert.style.zIndex = "-1";
+            let successBtn = document.getElementById('success-img');
+            successBtn.onclick = function(){
+                sucessAlert.style.zIndex = "-1";
+            }
         }
     }
 });
