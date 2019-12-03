e5ff315879e4b8a924ecf9fb6ba23376159e27b1
Ferdo Vukojević <f.vukojevic"globalsoft.ba>
2019-12-03 09:44:32 +0100

    functions php oop

--- wp-content/themes/enterwellTask/functions.php
+++ wp-content/themes/enterwellTask/functions.php
@@ -3,46 +3,60 @@
 require(__DIR__. '/mapper/PrizeEntryMapper.php');
 require(__DIR__. '/model/Writer.php');
 
-/**
- * @return void
- */
-function enterwellTask_resources() {
-    wp_enqueue_style('style', get_stylesheet_uri());
-}
 
-/**
- * @return void
- */
-function enterwellTask_script() {
-    wp_enqueue_script( 'main', get_template_directory_uri() . '/dist/main.js');
-}
-
-/**
- * @return void
- */
-function enterwell_form_submit()
+class Functions
 {
-    $post = $_POST;
-    $post['img'] = $_FILES['img'];
+    private $theme_name;
+    private $theme_version;
 
-    $date = new DateTime();
-    $timestamp = (string)$date->getTimestamp();
-    $post['img']['name'] = $timestamp . '_' . $post['img']['name'];
-
-    $mapper = new PrizeEntryMapper;
-    $data_array = $mapper->mapUsersData($post);
-
-    $writer = new Writer;
-    $response = $writer->storePrizeEntry($data_array);
-    if($response = 'success') {
-        $writer->storeImgToUploadsFolder($post['img']);
+    public function __construct($theme_name, $theme_version)
+    {
+        $this->theme_name = $theme_name;
+        $this->theme_version = $theme_version;
+        $this->theme_setup();
+    }
+    private function theme_setup()
+    {
+        add_action('wp_enqueue_scripts', array($this, 'enqueue_styles'));
+        add_action('wp_enqueue_scripts', array($this, 'enqueue_scripts'));
+        add_action('admin_post_nopriv_enterwell_application_form', 'enterwell_form_submit' );
+        add_action('admin_post_enterwell_application_form', 'enterwell_form_submit' );
     }
 
-    wp_redirect(home_url() . '?msg=' . $response);
+    public function enqueue_styles()
+    {
+        wp_enqueue_style( $this->theme_name . '_main_css', get_stylesheet_directory_uri() . '/dist/css/main.min.css', array(), $this->theme_version );
+        wp_enqueue_style('style', get_stylesheet_uri());
+    }
+
+    public function enqueue_scripts()
+    {
+        wp_enqueue_script( $this->theme_name . '_main_js', get_stylesheet_directory_uri() . '/dist/main.js', array(), $this->theme_version );
+        wp_enqueue_script( 'main', get_template_directory_uri() . '/dist/main.js');
+    }
+
+    public function enterwell_form_submit()
+    {
+        $post = $_POST;
+        $post['img'] = $_FILES['img'];
+
+        $date = new DateTime();
+        $timestamp = (string)$date->getTimestamp();
+        $post['img']['name'] = $timestamp . '_' . $post['img']['name'];
+
+        $mapper = new PrizeEntryMapper;
+        $data_array = $mapper->mapUsersData($post);
+
+        $writer = new Writer;
+        $response = $writer->storePrizeEntry($data_array);
+        if($response === 'success') {
+            $writer->storeImgToUploadsFolder($post['img']);
+        }
+
+        wp_redirect(home_url() . '?msg=' . $response);
+    }
 }
 
 
-add_action('wp_enqueue_scripts','enterwellTask_script');
-add_action('wp_enqueue_style', 'enterwellTask_resources');
-add_action('admin_post_nopriv_enterwell_application_form', 'enterwell_form_submit' );
-add_action('admin_post_enterwell_application_form', 'enterwell_form_submit' );
+$functions = new Functions('my-theme', '1.0.0');
+
