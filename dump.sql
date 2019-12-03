8680754ca4f06de410ab26cda98fb63d3a056f1c
Ferdo Vukojević <f.vukojevic"globalsoft.ba>
2019-12-03 13:22:38 +0100

    functions php oop

--- dump.sql
+++ dump.sql
@@ -98,8 +98,8 @@ CREATE TABLE `wp_options` (
 --
 
 INSERT INTO `wp_options` (`option_id`, `option_name`, `option_value`, `autoload`) VALUES
-(1, 'siteurl', 'http://localhost/wordpress', 'yes'),
-(2, 'home', 'http://localhost/wordpress', 'yes'),
+(1, 'siteurl', 'http://localhost/EnterwellTask', 'yes'),
+(2, 'home', 'http://localhost/EnterwellTask', 'yes'),
 (3, 'blogname', 'Enterwell TASK', 'yes'),
 (4, 'blogdescription', 'Just another WordPress site', 'yes'),
 (5, 'users_can_register', '0', 'yes'),
